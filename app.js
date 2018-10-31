2ba392481064f6cbd2b18216c362a1bafcb6ab36
carissa morrow <carissamorrow@hotmail.com>
2018-10-31 15:26:18 -0600

    gameadditions

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-function changeTitle(name) {
-  var titleElem = document.getElementById("title")
-  titleElem.innerText = name
+let computerChoice = "Rock"
+
+function play(playerChoice) {
+  var playerChoiceElem = document.getElementById("pchoice")
+  var computerChoiceElem = document.getElementById("cchoice")
+  var results = document.getElementById("results")
+  computerChoiceElem.innerText = computerChoice
+  playerChoiceElem.innerText = playerChoice
+  if (playerChoice == "Paper") {
+    if (computerChoice == "Rock") {
+      results.innerText = "Player Win"
+
+    }
+  }
+  if (playerChoice == "Scissors") {
+    if (computerChoice == "Rock") {
+      results.innerText = "Computer Win"
+    }
+  }
+  if (playerChoice == "Rock") {
+    if (computerChoice == "Scissors") {
+      results.innerText = "Player Win"
+    }
+  }
 }
